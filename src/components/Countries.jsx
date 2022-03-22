000a78dbc4105c3380457fc66c38119b3fbf0b8b
maxlestage <maxlestage@icloud.com>
2022-03-22 16:04:17 +0100

    Clean code

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -7,57 +7,63 @@ function Countries({ allCountry }) {
     setClicked(!clicked);
   }
 
-  return (
-    <>
-      {allCountry.length > 0 ? (
-        <ul>
-          {allCountry.map((n) => {
-            if (allCountry) {
-              if (allCountry.length === 1) {
-                return (
-                  <>
-                    <ShowCountry
-                      key={n.name.common}
-                      title={n.name.common}
-                      capital={n.capital}
-                      area={n.area}
-                      languages={n.languages}
-                      flag={n.flags.png}
-                      alt={n.name.common}
-                    />
-                  </>
-                );
-              } else if (allCountry.length > 1 && allCountry.length < 10) {
-                return (
-                  <div key={n.name.common}>
-                    {n.name.common}{" "}
-                    <button onClick={handleMoreClick}>
-                      {clicked ? "Hide" : "Show"}
-                    </button>
-                    {clicked && (
-                      <ShowCountry
-                        key={n.name.common}
-                        title={n.name.common}
-                        capital={n.capital}
-                        area={n.area}
-                        languages={n.languages}
-                        flag={n.flags.png}
-                        alt={n.name.common}
-                      />
-                    )}
-                  </div>
-                );
-              } else {
-                return <div key={n.name.common}>{n.name.common}</div>;
-              }
-            }
-          })}
-        </ul>
-      ) : (
-        <p>Nothing</p>
-      )}
-    </>
-  );
+  if (allCountry.length === 1) {
+    return (
+      <>
+        {allCountry.map((n) => {
+          return (
+            <ShowCountry
+              key={n.name.common}
+              title={n.name.common}
+              capital={n.capital}
+              area={n.area}
+              languages={n.languages}
+              flag={n.flags.png}
+              alt={n.name.common}
+            />
+          );
+        })}
+      </>
+    );
+  }
+
+  if (allCountry.length > 1 && allCountry.length < 10) {
+    return (
+      <>
+        {allCountry.map((n) => {
+          return (
+            <div key={n.name.common}>
+              {n.name.common}{" "}
+              <button onClick={handleMoreClick}>
+                {clicked ? "Hide" : "Show"}
+              </button>
+              {clicked && (
+                <ShowCountry
+                  key={n.name.common}
+                  title={n.name.common}
+                  capital={n.capital}
+                  area={n.area}
+                  languages={n.languages}
+                  flag={n.flags.png}
+                  alt={n.name.common}
+                />
+              )}
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
+  if (allCountry) {
+    return (
+      <>
+        {allCountry.map((n) => {
+          return <div key={n.name.common}>{n.name.common}</div>;
+        })}
+      </>
+    );
+  }
 }
 
 export default Countries;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,17 +6,13 @@ function Search({ countries }) {
 
   const submittNewSearch = (e) => {
     e.preventDefault();
-    console.log("button clicked", e.target);
   };
 
   const handlerSearch = (e) => {
-    console.log(e.target.value);
     setSearch(e.target.value);
   };
 
   const filterByName = countries.filter((country) => {
-    // const name = country.map((n) => n.name.common);
-    // console.log(name);
     return country.name.common.toLowerCase().includes(search.toLowerCase());
   });
 
